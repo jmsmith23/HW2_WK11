4590176f243bd6a48cb8b07588b7150f3d3892bc
jmsmith23 <smitjeff23@gmail.com>
2023-06-12 21:16:53 -0500

    Fix date function

--- models/todo.js
+++ models/todo.js
@@ -4,7 +4,7 @@ const todoSchema = new mongoose.Schema({
   title: { type: String, required: true },
   description: String,
   completed: { type: Boolean, default: false },
-  createdAt: { type: Date, default: Date.now },
+  createdAt: { type: Date, default: Date.now() },
 });
 
 const Todo = mongoose.model('Todo', todoSchema);
